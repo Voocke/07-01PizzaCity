1f272503cf773b8eee8d92d3ed0023baf905bbec
Ivan Timofeev <je8byu@gmail.com>
2025-11-11 16:15:42 +0300

    building structure for new city

--- src/main/main.kt
+++ src/main/main.kt
@@ -11,15 +11,22 @@ fun main() {
         350.5, 449.9,
         0.0
     )
+    val pizzaKazan = PizzaKazan(
+        299.0,420.0,
+        279.9, 389.5,
+        175.0
+    )
+
     var currentPizzaCity: PizzaCity
 
     while (true) {
         println("Добрый день! Выберите город")
-        println("1. Москва\n2. Санкт-Петербург\n0. Выход из программы")
+        println("1. Москва\n2. Санкт-Петербург\n3. Казань\n0. Выход из программы")
 
         currentPizzaCity = when (readln()) {
             "1" -> pizzaMoscow
             "2" -> pizzaPeter
+            "3" -> pizzaKazan
             "0" -> break
             else -> {
                 println("Error")
@@ -56,7 +63,14 @@ private fun selectPizza(currentPizzaCity: PizzaCity) {
             currentPizzaCity.tyroleanPizzaSale()
             selectAddService(currentPizzaCity)
         }
-        "0" -> currentPizzaCity.showStatistics()
+        "0" -> {
+            currentPizzaCity.showStatistics()
+            when (currentPizzaCity) {
+                is PizzaPeter -> println()
+                is PizzaMoscow -> println()
+                is PizzaKazan -> println()
+            }
+        }
         else -> {
             println("ERROR")
             selectPizza(currentPizzaCity)
@@ -67,8 +81,13 @@ private fun selectAddService(currentPizzaCity: PizzaCity) {
     when (currentPizzaCity) {
         is CheckPhoto -> currentPizzaCity.showCheckPhoto()
         is Drink -> {
-            if(currentPizzaCity.drinkSale())
+            if (currentPizzaCity.drinkSale())
                 currentPizzaCity.pizzaWithCoffee(pizzaChoice)
+            else
+                return
+        }
+        is Sauce -> {
+            //TODO написать функцию по продаже соусов
         }
     }
 }
