8177fda8217e909eaeb4204aa52466e1353fbcec
Ivan Timofeev <je8byu@gmail.com>
2025-11-11 16:15:16 +0300

    changed structure

--- src/main/Drink.kt
+++ src/main/Drink.kt
@@ -2,18 +2,5 @@ package main
 
 interface Drink {
     fun drinkSale(): Boolean
-    var countNeapolitanWithCoffee: Int
-    var countRomanWithCoffee: Int
-    var countSicilianWithCoffee: Int
-    var countTyroleanWithCoffee: Int
-    fun pizzaWithCoffee(pizzaChoice: Int) {
-        when (pizzaChoice){
-            1 -> countNeapolitanWithCoffee++
-            2 -> countRomanWithCoffee++
-            3 -> countSicilianWithCoffee++
-            4 -> countTyroleanWithCoffee++
-        }
-
-    }
 
 }
